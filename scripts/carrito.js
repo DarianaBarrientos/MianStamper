57e6cdc02793d1182b648f99476a1750755c67b6
Daniel Guasamucaro <darianabarrientos09@gmail.com>
2023-10-25 20:18:03 -0300

    se le dio funcionalidad al carrito

---
+++ scripts/carrito.js
@@ -0,0 +1,126 @@
+let carrito = localStorage.getItem('productos-agregados');
+carrito = JSON.parse(carrito);
+
+const contenedorCarritoVacio = document.querySelector('#carrito-vacio');
+const contenedorCarritoProductos = document.querySelector('#contenedor-carrito-productos');
+const contenedorCarritoAcciones = document.querySelector('#carrito-acciones');
+const contenedorCarritoAccionComprar = document.querySelector('#carrito-accion-comprar')
+const contenedorCarritoComprado = document.querySelector('#carrito-comprado');
+let carritoEliminar = document.querySelectorAll('.carrito-eliminar');
+const botonVaciar = document.querySelector('#boton-vaciar');
+const contenedorCarritoTotal = document.querySelector('#carrito-accion-total');
+const botonComprar = document.querySelector('#carrito-accion-comprar');
+const contenedorCarritoTotalIva = document.querySelector('#carrito-accion-total-iva');
+
+const IVA = 1.21;
+let totalCalculado;
+
+function cargarProductosEnCarrito() {
+
+    if(carrito && carrito.length > 0) {
+    
+    contenedorCarritoVacio.classList.add('oculto');
+    contenedorCarritoProductos.classList.remove('oculto');
+    contenedorCarritoAcciones.classList.remove('oculto');
+    contenedorCarritoAccionComprar.classList.remove('oculto');
+    contenedorCarritoComprado.classList.add('oculto'); 
+
+    contenedorCarritoProductos.innerHTML = '';
+
+    carrito.forEach(producto => {
+
+        const div = document.createElement('div');
+        div.classList.add('carrito-producto');
+        div.innerHTML = `
+            <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.nombre}">
+            <div class="carrito-producto-titulo">
+                <small>Producto</small>
+                <h5>${producto.nombre}</h5>
+            </div>
+            <div class="carrito-producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito-producto-precio">
+                <small>Precio</small>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+                <small>Subtotal</small>
+                <p>$${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito-eliminar" id="${producto.id}"><i class="bi bi-trash3"></i></button>
+        `;
+
+       contenedorCarritoProductos.append(div);
+
+    }) 
+    }else {
+
+        contenedorCarritoVacio.classList.remove('oculto');
+        contenedorCarritoProductos.classList.add('oculto');
+        contenedorCarritoAcciones.classList.add('oculto');
+        contenedorCarritoAccionComprar.classList.add('oculto');
+        contenedorCarritoComprado.classList.add('oculto'); 
+
+    } 
+
+    actualizarBotonesEliminar();
+    totalFinal();
+    totalIva();
+}
+
+cargarProductosEnCarrito();
+
+
+function actualizarBotonesEliminar() {
+    botonesEliminar = document.querySelectorAll('.carrito-eliminar');
+  
+    botonesEliminar.forEach(boton => {
+      boton.addEventListener('click', eliminarProducto);
+    });
+  }
+
+
+function eliminarProducto(e) {
+    const idBoton = e.currentTarget.id;
+    const posicion = carrito.findIndex(producto => producto.id === idBoton);
+    carrito.splice(posicion, 1);
+
+    cargarProductosEnCarrito();
+
+    localStorage.setItem('productos-agregados', JSON.stringify(carrito));
+}
+
+
+botonVaciar.addEventListener('click', vaciarCarrito);
+function vaciarCarrito() {
+    carrito.length = 0;
+    localStorage.setItem('productos-agregados', JSON.stringify(carrito));
+    cargarProductosEnCarrito();
+}
+
+
+
+function totalFinal() {
+    totalCalculado = carrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
+    total.innerHTML = `$${totalCalculado}`;
+}
+
+botonComprar.addEventListener('click', comprarCarrito);
+function comprarCarrito() {
+    carrito.length = 0;
+    localStorage.setItem('productos-agregados', JSON.stringify(carrito));
+
+    contenedorCarritoVacio.classList.add('oculto');
+    contenedorCarritoProductos.classList.add('oculto');
+    contenedorCarritoAcciones.classList.add('oculto');
+    contenedorCarritoAccionComprar.classList.add('oculto');
+    contenedorCarritoComprado.classList.remove('oculto'); 
+}
+
+function totalIva() {
+    const totalConIva = totalCalculado * IVA;
+    iva.innerHTML = `$${totalConIva}`;
+}
+
--- scripts/productos.js
+++ scripts/productos.js
@@ -1,18 +1,117 @@
 //compras (carrito)
 //sumar => los precios
 
-/* const IVA = 1.21;
-
 const productos = [
-  { nombre: 'taza', precio: 1000 },
-  { nombre: 'vaso', precio: 1300 },
-  { nombre: 'copa', precio: 1500 },
-  { nombre: 'gorra', precio: 2000 },
-  { nombre: 'remera', precio: 3000 },
-  { nombre: 'termo', precio: 2500 },
+  { 
+    id: 'producto-01',
+    nombre: 'taza', 
+    imagen: '../assets/img/taza.png',
+    precio: 1000 
+  },
+  { 
+    id: 'producto-02',
+    nombre: 'vaso', 
+    imagen: '../assets/img/vaso.png',
+    precio: 1300 
+  },
+  { id: 'producto-03',
+    nombre: 'copa',
+    imagen: '../assets/img/copa.png',
+    precio: 1500 
+  },
+  { id: 'producto-04',
+    nombre: 'gorra', 
+    imagen: '../assets/img/gorra.png',
+    precio: 2000 
+  },
+  { id: 'producto-05',
+    nombre: 'remera',
+    imagen: '../assets/img/remera.png',
+    precio: 3000 
+  },
+  { id: 'producto-06',
+    nombre: 'termo',
+    imagen: '../assets/img/termo.png',
+    precio: 2500 
+  },
 ];
 
-const carrito = [];
+const contenedorProductos = document.querySelector('#contenedor-productos');
+let botonesAgregar = document.querySelectorAll('.productos__button');
+
+function cargarEstructuraProductos () {
+
+  productos.forEach(producto => {
+
+    const section = document.createElement('section');
+    section.classList.add('productos');
+    section.innerHTML = `
+      <article id="container-productos" class="productos__container">
+        <div>
+            <img class="productos__imagen" src="${producto.imagen}" alt="${producto.nombre}">
+        </div>
+        <div class="productos__info">
+            <p>${producto.nombre}</p>
+        </div>
+        <div class="productos__precio">
+            <p>$${producto.precio}</p>
+        </div>
+        <div class="boton-producto-container">
+            <button  type="button" class="btn btn-secondary productos__button" id="${producto.id}">agregar</button>
+        </div>
+      </article>
+    `;
+
+    contenedorProductos.append(section);
+
+  })
+
+  actualizarBotonesAgregar();
+
+}
+
+cargarEstructuraProductos();
+
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll('.productos__button');
+
+  botonesAgregar.forEach(boton => {
+    boton.addEventListener('click', agregarAlCarrito);
+  });
+}
+
+let carrito;
+let carritoLocalStorage = localStorage.getItem('productos-agregados');
+
+if (carritoLocalStorage) {
+  carrito = JSON.parse(carritoLocalStorage);
+} else {
+  carrito = [];
+}
+
+function agregarAlCarrito(e) {
+  
+  const idBoton = e.target.id;
+  const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+  if(carrito.some(producto => producto.id === idBoton)) {
+
+    const posicion = carrito.findIndex(producto => producto.id === idBoton);
+    carrito[posicion].cantidad++;
+
+  } else {
+
+    productoAgregado.cantidad = 1;
+    carrito.push(productoAgregado);
+
+  }
+
+  localStorage.setItem('productos-agregados', JSON.stringify(carrito));
+
+}
+
+
+/* const carrito = [];
 let respuestaUsuario = prompt('¿Quieres realizar un pedido?');
 while (respuestaUsuario?.trim().toUpperCase() === "SI") {
   let respuestaIngresada = prompt('Ingrese el nombre del producto que desea agregar al carrito')?.trim().toLowerCase();
@@ -39,4 +138,5 @@ while (respuestaUsuario?.trim().toUpperCase() === "SI") {
     alert(`Su pedido ya fue hecho y el total con IVA es de $ ${totalSinIVA * IVA} \n ¡Gracias por su compra!`);
     break;
   }
-}  */
+}
+ */
