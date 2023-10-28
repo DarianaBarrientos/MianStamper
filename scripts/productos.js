594c7d90a6ce5e63ed1f6fd2ad7de54d4488bc7e
Daniel Guasamucaro <darianabarrientos09@gmail.com>
2023-10-27 21:04:30 -0300

    se eliminan los comments dentro de producto.js

--- scripts/productos.js
+++ scripts/productos.js
@@ -109,34 +109,3 @@ function agregarAlCarrito(e) {
   localStorage.setItem('productos-agregados', JSON.stringify(carrito));
 
 }
-
-
-/* const carrito = [];
-let respuestaUsuario = prompt('¿Quieres realizar un pedido?');
-while (respuestaUsuario?.trim().toUpperCase() === "SI") {
-  let respuestaIngresada = prompt('Ingrese el nombre del producto que desea agregar al carrito')?.trim().toLowerCase();
-
-  // Valida que exista el producto que ingreso la persona para no enviar un null al carrito
-  const existeProducto = productos.some((elemento) => elemento.nombre === respuestaIngresada?.trim().toLowerCase());
-  if (!existeProducto) {
-    alert('Ese producto no existe en nuestro stock');
-  }
-  else {
-  // Agregamos el producto seleccionado al carrito
-  carrito.push(productos.find((elemento) => elemento.nombre === respuestaIngresada?.trim().toLowerCase()));
-  console.log('carrito', carrito);
-  }
-
-  respuestaIngresada = prompt('¿Quieres realizar otro pedido?');
-  if (respuestaIngresada?.trim().toUpperCase() === "SI") {
-    continue
-  }
-  else {
-    // Sacamos el total
-    const totalSinIVA = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0);
-    console.log('total sin IVA', totalSinIVA);
-    alert(`Su pedido ya fue hecho y el total con IVA es de $ ${totalSinIVA * IVA} \n ¡Gracias por su compra!`);
-    break;
-  }
-}
- */
